df5d1e5a54dd802e5002b5c9df792f32d2923128
Robin Miller <rmillerpdx@gmail.com>
2017-09-11 13:48:00 -0700

    Did first 4 TODO's

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -3,10 +3,17 @@ var app = app || {};
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
+  articleController.init = () => {
+    $('#articles').hide();
+    $('#about').hide();
+    
+    app.articleView.initIndexPage();
 
-
+    $('#articles').show();
+    //does order matter?
+  }
   module.articleController = articleController;
 })(app);
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,17 @@
 'use strict';
 var app = app || {};
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
+// DONE TODO: Configure routes for this app with page.js, by registering each URL your app can handle, 
+//linked to a single controller function to handle it. Note that these routes do not 
+//need to wrapped in an IIFE.
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+page( '/', function () {console('@ index page');} );
+page( '/about', app.aboutController.init );
+page( '/article', app.articleController.init );
+// TO DO MAYBE make a thing for New controller page( '/new', app.initNewArticlePage)
+
+
+// TODO: What function do you call to activate page.js? Fire it off now, to execute.
+// Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+
+page.start();
